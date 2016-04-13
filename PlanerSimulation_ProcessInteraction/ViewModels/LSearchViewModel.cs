b562733b7c60aa1821043de00908d0f9dad3e470
bielik <bielik20@gmail.com>
2016-04-13 22:17:32 +0200

    28th Commit
    After adding way to implement confidence interval and before changes to
    Parralel.

--- PlanerSimulation_ProcessInteraction/ViewModels/LSearchViewModel.cs
+++ PlanerSimulation_ProcessInteraction/ViewModels/LSearchViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PlanerSimulation_ProcessInteraction.Models;
 using PlanerSimulation_ProcessInteraction.Statistics;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,9 @@ namespace PlanerSimulation_ProcessInteraction.ViewModels
     {
         NormalViewModel Overwatch { get; set; }
         public double[] Lambdas { get; private set; }
-        public List<KeyVal<string,LStats.Results>> ResultsList { get; private set; }
+        public KeyVal<double, LStats.Results>[] ResultsList { get; private set; }
+        private ConcurrentBag<LStats.Results>[] AverageList { get; set; }
+        public List<KeyVal<double, double>>[] ConfidenceInterval { get; set; }
         public double Size { get; set; } = 34;
 
         public class KeyVal<TKey, TVal>
@@ -36,29 +39,26 @@ namespace PlanerSimulation_ProcessInteraction.ViewModels
         public override void Simulate()
         {
             Lambdas = new double[Overwatch.NumOfLambdas];
-            ResultsList = new List<KeyVal<string, LStats.Results>>();
-            Action[] mySimulations = new Action[Overwatch.NumOfLambdas];
+            ResultsList = new KeyVal<double, LStats.Results>[Overwatch.NumOfLambdas];
+            AverageList = new ConcurrentBag<LStats.Results>[Overwatch.NumOfLambdas];
             for (int i = 0; i < Overwatch.NumOfLambdas; i++)
             {
                 Lambdas[i] = Math.Round(Overwatch.Lambda + (Overwatch.NumOfLambdas / 2 - i) * Overwatch.LambdaSpan, 9);
                 CreateList(i);
 
                 var _temp = i;
-                mySimulations[i] = new Action(() => SetLambda(_temp));
+                Parallel.For(0, Overwatch.NumOfTrials, _ => RunSimulation(i));
             }
-            Parallel.Invoke(mySimulations);
+            FindConfidenceInterval();
             OnPropertyChanged("ResultsList");
             OnPropertyChanged("Size");
+            OnPropertyChanged("ConfidenceInterval");
+            OnPropertyChanged("Lambdas");
         }
 
         private void SetLambda(int index)
         {
-            Action[] mySimulations = new Action[Overwatch.NumOfTrials];
-            for (int i = 0; i < Overwatch.NumOfTrials; i++)
-            {
-                mySimulations[i] = new Action(() => RunSimulation(index));
-            }
-            Parallel.Invoke(mySimulations);
+            Parallel.For(0, Overwatch.NumOfTrials, i => RunSimulation(index));
         }
 
         private void RunSimulation(int index)
@@ -72,11 +72,37 @@ namespace PlanerSimulation_ProcessInteraction.ViewModels
         private void UpdateList(int index, LStats _stats)
         {
             ResultsList[index].Val += _stats.MyResults / Overwatch.NumOfTrials;
+            AverageList[index].Add(_stats.MyResults);
         }
 
         private void CreateList(int index)
         {
-            ResultsList.Add(new KeyVal<string, LStats.Results>(Lambdas[index].ToString() + "  ", new LStats.Results(0)));
+            ResultsList[index] = new KeyVal<double, LStats.Results>(Lambdas[index], new LStats.Results(0));
+            AverageList[index] = new ConcurrentBag<LStats.Results>();
+        }
+
+        private void FindConfidenceInterval()
+        {
+            var standardDeviation = new double[Overwatch.NumOfLambdas];
+            for (int i = 0; i < Overwatch.NumOfLambdas; i++)
+            {
+                standardDeviation[i] = 0;
+                foreach (var item in AverageList[i])
+                {
+                    standardDeviation[i] += Math.Pow(item.CPUAwaitTime - ResultsList[i].Val.CPUAwaitTime, 2);
+                }
+                standardDeviation[i] = Math.Sqrt(standardDeviation[i] / (Overwatch.NumOfTrials - 1));
+            }
+
+            ConfidenceInterval = new List<KeyVal<double, double>>[Overwatch.NumOfLambdas];
+            for (int i = 0; i < Overwatch.NumOfLambdas; i++)
+            {
+                ConfidenceInterval[i] = new List<KeyVal<double, double>>();
+                ConfidenceInterval[i].Add(new KeyVal<double, double>(Lambdas[i], 43.6));
+                ConfidenceInterval[i].Add(new KeyVal<double, double>(Lambdas[i], 53.4));
+            }
+            ConfidenceInterval[2][1].Val = 40;
+
         }
     }
 }
